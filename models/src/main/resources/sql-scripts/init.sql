17ae48c22963bb7a915080640be887b28b02f6d8
Domen Mohorcic <d.mohorcic@gmail.com>
2023-01-06 13:34:26 +0100

    Added correct coordinates

--- models/src/main/resources/sql-scripts/init.sql
+++ models/src/main/resources/sql-scripts/init.sql
@@ -1,6 +1,6 @@
-INSERT INTO deliveryman_metadata (name, surname, vehicle, lat, lng, hourlyPay, transportPrice, created) VALUES ('Franc', 'Novak', 'car', 10, 20, 8, 1, TIMESTAMP '2001-01-01T12:00:00Z');
-INSERT INTO deliveryman_metadata (name, surname, vehicle, lat, lng, hourlyPay, transportPrice, created) VALUES ('Janez', 'Novak', 'car', 10, 20, 7, 1.1, TIMESTAMP '2002-02-02T12:00:00Z');
-INSERT INTO deliveryman_metadata (name, surname, vehicle, lat, lng, hourlyPay, transportPrice, created) VALUES ('Marko', 'Novak', 'bike', 10, 20, 12, 0.1, TIMESTAMP '2003-03-03T12:00:00Z');
-INSERT INTO deliveryman_metadata (name, surname, vehicle, lat, lng, hourlyPay, transportPrice, created) VALUES ('Andrej', 'Novak', 'bike', 10, 20, 11, 0.15, TIMESTAMP '2004-04-04T12:00:00Z');
-INSERT INTO deliveryman_metadata (name, surname, vehicle, lat, lng, hourlyPay, transportPrice, created) VALUES ('Ivan', 'Novak', 'foot', 10, 20, 9, 0, TIMESTAMP '2005-05-05T12:00:00Z');
-INSERT INTO deliveryman_metadata (name, surname, vehicle, lat, lng, hourlyPay, transportPrice, created) VALUES ('Anton', 'Novak', 'foot', 10, 20, 9, 0, TIMESTAMP '2006-06-06T12:00:00Z');
+INSERT INTO deliveryman_metadata (name, surname, vehicle, lat, lng, hourlyPay, transportPrice, created) VALUES ('Franc', 'Novak', 'car', 46.078893, 14.493639, 8, 1, TIMESTAMP '2001-01-01T12:00:00Z');
+INSERT INTO deliveryman_metadata (name, surname, vehicle, lat, lng, hourlyPay, transportPrice, created) VALUES ('Janez', 'Novak', 'car', 46.033388, 14.499476, 7, 1.1, TIMESTAMP '2002-02-02T12:00:00Z');
+INSERT INTO deliveryman_metadata (name, surname, vehicle, lat, lng, hourlyPay, transportPrice, created) VALUES ('Marko', 'Novak', 'bike', 46.031481, 14.529345, 12, 0.1, TIMESTAMP '2003-03-03T12:00:00Z');
+INSERT INTO deliveryman_metadata (name, surname, vehicle, lat, lng, hourlyPay, transportPrice, created) VALUES ('Andrej', 'Novak', 'bike', 46.054120, 14.499819, 11, 0.15, TIMESTAMP '2004-04-04T12:00:00Z');
+INSERT INTO deliveryman_metadata (name, surname, vehicle, lat, lng, hourlyPay, transportPrice, created) VALUES ('Ivan', 'Novak', 'foot', 46.064216, 14.528970, 9, 0, TIMESTAMP '2005-05-05T12:00:00Z');
+INSERT INTO deliveryman_metadata (name, surname, vehicle, lat, lng, hourlyPay, transportPrice, created) VALUES ('Anton', 'Novak', 'foot', 46.039674, 14.474038, 9, 0, TIMESTAMP '2006-06-06T12:00:00Z');
